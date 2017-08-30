05cd1ab9104b9ed82e6a2e28450924f5afcb3c41
Dmitry Birin <dmitry.birin@gmail.com>
2017-08-30 20:55:10 +0300

    Home page expended Getting there with async/await

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -2,11 +2,17 @@ import Page from './Page'
 
 export default class HomePage extends Page {
 
-    selector = element(by.css("#mainMenu"))
- 
+    selector = by.css('div.rci_status_wrapper')
+    
+    mainMenuLinks = by.css('ul#mainMenu a')
+    
     get = () => {
         this.maximize()
-        this.getRelativeUrl('/')
+        return this.getRelativeUrl('/')
     }
 
+    getLinkTexts = async () => this.getElementsTexts(this.mainMenuLinks)
+
+    clickFirstLinkByName = async (name) => {let els = await this.getElementsByTextInArea(this.mainMenuLinks, name); els[0].click()}
+    
 }
--- test.js
+++ test.js
@@ -1,27 +1,27 @@
 import HomePage from './pages/HomePage'
+import PaymentsPage from './pages/PaymentsPage'
 
-describe('Protractor Demo Test', function() {
+const homePage = new HomePage()
+
+
+describe('Protractor Demo Test', () => {
     
-    const mainMenuLinks = 'ul#mainMenu a'
+    describe("homepage", () => {
 
-    const page = new HomePage()
-    page.get()
-  
-    it('should have a right title', 
-        () => 
-        expect(browser.getTitle())
-                        .toEqual('Лучший онлайн-банк. Кредитные и дебетовые карты с доставкой на дом.')
-    )
+      describe("when on homepage", () => {
+        
+        beforeAll( async () => {
+          await homePage.get()
+        })
+    
+        const pauseFunc = (t) => new Promise((res,rej) => setTimeout(()=>res('sup'), t*1000))
 
-    const getElementsByTextInArea = async (areaSelector, text) => await element.all(by.css(areaSelector)).filter(el => el.getText().then(elText => elText.toLowerCase() === text.toLowerCase()))
+        it("click", async () => {
+          await homePage.clickFirstLinkByName('Платежи')
+          let page = new PaymentsPage()
+          await page.waitUntilDisplayed()
+        })
 
-    it('some test', async function () {
-        let els = await getElementsByTextInArea(mainMenuLinks, 'Платежи')
-        let el = await els[0].click()
-        console.log(await browser.getCurrentUrl())
+      })
     })
-
   })
-
-// var until = protractor.ExpectedConditions
-// browser.wait(until.presenceOf(elem), 5000, 'Element taking too long to appear in the DOM')
