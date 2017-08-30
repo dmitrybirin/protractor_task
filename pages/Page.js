bd822362fbc91ae4b205d631711cc9deef8373d6
Dmitry Birin <dmitry.birin@gmail.com>
2017-08-30 20:56:05 +0300

    Page refactored using by as selector

--- pages/Page.js
+++ pages/Page.js
@@ -28,20 +28,25 @@ export default class Page {
      */
     isDisplayed = () => {
       this.selectorGuard()
-      const until = protractor.ExpectedConditions
-      return until.visibilityOf(this.selector)()
+      return protractor.ExpectedConditions.visibilityOf(element(this.selector))
     }
-  
+
+    isCondition = async (condition) => {
+      if (!condition) throw new TypeError('condition function must present to get condition status')
+      else return condition()
+    }
+
+
     /**
      * Wait until the current page is displayed.
      */
-    waitUntilDisplayed = () => {
+    waitUntilDisplayed = async () => {
+      let el = await element(this.selector)
       this.selectorGuard()
-  
-      browser.wait(
-        this.isDisplayed,
+      return browser.wait(
+        protractor.ExpectedConditions.visibilityOf(el),
         waitUntilDisplayedTimeout,
-        `Failed while waiting for "${this.selector.locator()}" of 'Page' class 
+        `Failed while waiting for "${el.locator()}" of 'Page' class 
         '${this.constructor.name}' to display.`
       )
     }
@@ -49,9 +54,28 @@ export default class Page {
     getRelativeUrl = (relativeUrl) => {
         browser.baseUrl = BaseUrl
         browser.get(relativeUrl)
-        this.waitUntilDisplayed()
+        return this.waitUntilDisplayed()
     }    
 
     maximize = () => browser.manage().window().maximize()
+
+
+    getElementsTexts = async (areaSelector) => {
+        const els = await element.all(areaSelector)
+        return Promise.all(els.map(el => el.getText().then(elText => elText.toLowerCase())))
+    }
+
+    getElementsByTextInArea = async (areaSelector, text) => {
+      const els = await element.all(areaSelector)
+      Array.prototype.mapAsync = function(fn) {
+          return Promise.all(this.map(fn))
+      }
+      Array.prototype.filterAsync = function(fn) {
+          return this.mapAsync(fn).then(_arr => this.filter((v, i) => !!_arr[i]))
+      }
+      let filtered = await els.filterAsync(async el => el.getText().then(elText => {console.log(elText); return elText.toLowerCase() === text.toLowerCase()}))
+      console.log(filtered.length)
+      return filtered
+    }
 
   }
